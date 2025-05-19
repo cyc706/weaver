dbc653427f94781cc574d9f718bfd23f5e4427b3
chenyancheng <cyckoo@qq.com>
2025-05-19 20:43:51 +0800

    feat: 增加当前时间

--- src/app/ybs/page.tsx
+++ src/app/ybs/page.tsx
@@ -1,34 +1,44 @@
 import { getIndexData } from "@/lib/stock";
+import dayjs from "dayjs";
 export const dynamic = "force-dynamic";
 
 export default async function YBS() {
   const indices = await getIndexData();
 
   return (
-    <div className="flex p-2">
-      {indices.map((index) => (
-        <div key={index.symbol} className="flex-1">
-          <div className="text-sm font-semibold text-center">{index.name}</div>
-          <div
-            className={`text-lg font-bold text-center ${
-              index.percent >= 0 ? "text-red-600" : "text-green-600"
-            }`}
-          >
-            {index.current.toFixed(2)}
-          </div>
-          <div
-            className={`flex items-center justify-center gap-3 text-sm ${
-              index.chg >= 0 ? "text-red-600" : "text-green-600"
-            }`}
-          >
-            <div>{index.chg.toFixed(2)}</div>
-            <div>
-              {index.percent >= 0 ? "+" : ""}
-              {index.percent}%
+    <div>
+      <div className="flex p-2">
+        {indices.map((index) => (
+          <div key={index.symbol} className="flex-1">
+            <div className="text-sm font-semibold text-center">
+              {index.name}
+            </div>
+            <div
+              className={`text-lg font-bold text-center ${
+                index.percent >= 0 ? "text-red-600" : "text-green-600"
+              }`}
+            >
+              {index.current.toFixed(2)}
+            </div>
+            <div
+              className={`flex items-center justify-center gap-3 text-sm ${
+                index.chg >= 0 ? "text-red-600" : "text-green-600"
+              }`}
+            >
+              <div>{index.chg.toFixed(2)}</div>
+              <div>
+                {index.percent >= 0 ? "+" : ""}
+                {index.percent}%
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+
+
+      <div className="p-2">
+        <div className="text-gray-500 text-sm">当前时间: {dayjs().format('YYYY-MM-DD HH:mm:ss')}</div>
+      </div>
     </div>
   );
 }
